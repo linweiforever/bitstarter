d8337614a385a8ec3974a4936ce473835b1d366b
linweiforever <lx1111@gmail.com>
2013-07-09 04:33:00 +0000

    change web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-	fs.readFileSync(index.html);
+	fs.readFileSync('index.html');
 	response.send(buffer.toString('utf-8'));
 });
 
